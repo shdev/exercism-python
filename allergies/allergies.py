273b9287ec4a51ab93615fc3cc3b02b141b24eb9
Sebastian Holtz <mail@sh-dev.de>
2015-04-09 23:12:13 +0200

    allergies

---
+++ allergies/allergies.py
@@ -0,0 +1,14 @@
+class Allergies(object):
+    _allergie_items = ['eggs', 'peanuts', 'shellfish', 'strawberries',
+                       'tomatoes', 'chocolate', 'pollen', 'cats', ]
+
+    def __init__(self, score):
+        super(Allergies, self).__init__()
+        self.score = score
+        self.list = [self._allergie_items[idx]
+                     for idx in xrange(0, len(self._allergie_items))
+                     if self.score & 2 ** idx == 2 ** idx]
+
+    def is_allergic_to(self, item):
+        idx = self._allergie_items.index(item)
+        return self.score & 2 ** idx == 2 ** idx
