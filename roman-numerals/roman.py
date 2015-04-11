bcee0bfa5a4262f5ae178068b3537b12702e84ec
Sebastian Holtz <mail@sh-dev.de>
2015-04-11 21:38:44 +0200

    roman finshed

--- roman-numerals/roman.py
+++ roman-numerals/roman.py
@@ -1,39 +1,40 @@
-roman_digits = {
-    (1, 1): 'I',
-    (1, 5): 'V',
-    (2, 1): 'X',
-    (2, 5): 'L',
-    (3, 1): 'C',
-    (3, 5): 'D',
-    (4, 1): 'M',
-}
-
-
-roman_digits2 = {
-    1: {
+roman_digits = [
+    {
         1: 'I',
         5: 'V',
     },
-    2: {
+    {
         1: 'X',
         5: 'L',
     },
-    3: {
+    {
         1: 'C',
         5: 'D',
     },
-    4: {
+    {
         1: 'M',
     }
-}
+]
 
 
 def numeral(arabic):
-    arabic = reversed(str(arabic))
+    if arabic < 1:
+        raise ValueError('Romans don\'t like numbers smaller than one')
+    if arabic > 3000:
+        raise ValueError('Romans don\'t like numbers bigger than'
+                         ' three tausend')
 
+    arabic = list(reversed(str(arabic)))
+    roman = ''
 
-
-    for d in arabic:
-
-    else:
-        return 0
+    for i in range(len(arabic) - 1, - 1, - 1):
+        if arabic[i] in ['1', '2', '3']:
+            roman += roman_digits[i][1] * int(arabic[i])
+        elif arabic[i] == '4':
+            roman += roman_digits[i][1] + roman_digits[i][5]
+        elif arabic[i] in ['5', '6', '7', '8']:
+            roman += roman_digits[i][5] \
+                + roman_digits[i][1] * (int(arabic[i]) - 5)
+        elif arabic[i] == '9':
+            roman += roman_digits[i][1] + roman_digits[i + 1][1]
+    return roman
