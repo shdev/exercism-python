e52bfcf13941853971268c7d9a28cddf889b2505
Sebastian Holtz <mail@sh-dev.de>
2015-04-11 20:47:31 +0200

    roman wip

---
+++ roman-numerals/roman.py
@@ -0,0 +1,39 @@
+roman_digits = {
+    (1, 1): 'I',
+    (1, 5): 'V',
+    (2, 1): 'X',
+    (2, 5): 'L',
+    (3, 1): 'C',
+    (3, 5): 'D',
+    (4, 1): 'M',
+}
+
+
+roman_digits2 = {
+    1: {
+        1: 'I',
+        5: 'V',
+    },
+    2: {
+        1: 'X',
+        5: 'L',
+    },
+    3: {
+        1: 'C',
+        5: 'D',
+    },
+    4: {
+        1: 'M',
+    }
+}
+
+
+def numeral(arabic):
+    arabic = reversed(str(arabic))
+
+
+
+    for d in arabic:
+
+    else:
+        return 0
