59756093ee132fec2f47172bca3530d4a9cfae44
Sebastian Holtz <mail@sh-dev.de>
2015-04-11 23:18:17 +0200

    dynamic methods in python

--- space-age/space_age.py
+++ space-age/space_age.py
@@ -1,41 +1,35 @@
+import functools
+
+
+class PlanetError(Exception):
+    pass
+
+
 class SpaceAge(object):
-    _sec_to_year = 60 * 60 * 24 * 365.25
+    EARTHYEAR = 31557600
+    PERIODS = {
+        'mercury': 0.2408467,
+        'venus': 0.61519726,
+        'earth': 1.0,
+        'mars': 1.8808158,
+        'jupiter': 11.862615,
+        'saturn': 29.447498,
+        'uranus': 84.016846,
+        'neptune': 164.79132,
+    }
 
-    _earth_to_mercury = 0.2408467
-    _earth_to_venus = 0.61519726
-    _earth_to_mars = 1.8808158
-    _earth_to_jupiter = 11.862615
-    _earth_to_saturn = 29.447498
-    _earth_to_uranus = 84.016846
-    _earth_to_neptune = 164.79132
+    def __init__(self, age):
+        self.seconds = age
 
-    def __init__(self, seconds):
-        super(SpaceAge, self).__init__()
-        self.seconds = seconds
+    def on_planet(self, planet):
+        if planet not in self.PERIODS:
+            raise PlanetError(
+                "{} is not a planet in the solar system.".format(planet))
+        return round(self.seconds / self.PERIODS[planet] / self.EARTHYEAR, 2)
 
-    def _to_year(self):
-        return self.seconds / self._sec_to_year
-
-    def on_earth(self):
-        return round(self._to_year(), 2)
-
-    def on_mercury(self):
-        return round(self._to_year() / self._earth_to_mercury, 2)
-
-    def on_venus(self):
-        return round(self._to_year() / self._earth_to_venus, 2)
-
-    def on_mars(self):
-        return round(self._to_year() / self._earth_to_mars, 2)
-
-    def on_jupiter(self):
-        return round(self._to_year() / self._earth_to_jupiter, 2)
-
-    def on_saturn(self):
-        return round(self._to_year() / self._earth_to_saturn, 2)
-
-    def on_uranus(self):
-        return round(self._to_year() / self._earth_to_uranus, 2)
-
-    def on_neptune(self):
-        return round(self._to_year() / self._earth_to_neptune, 2)
+    def __getattr__(self, attr):
+        try:
+            planet = attr.split('_')[1]
+        except IndexError:
+            raise AttributeError("No such attribute '{}'".format(attr))
+        return functools.partial(self.on_planet, planet=planet)
