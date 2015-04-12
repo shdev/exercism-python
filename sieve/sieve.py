0392d5e592be908b2629426dec1afa8cfdc9c4aa
Sebastian Holtz <mail@sh-dev.de>
2015-04-12 16:43:09 +0200

    a better sieve implementation

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -1,10 +1,23 @@
-def sieve(n):
-    candidates = range(2, n + 1)
-    primes = []
-    while candidates:
-        prime = candidates[0]
-        del candidates[0]
-        primes += [prime]
-        candidates = [c for c in candidates if c % prime]
+__author__ = 'jetties'
+# a better implementation from @jetties
 
-    return primes
+
+def sieve(n):
+    # Simulate a bit array; each entry in [0, n] represents whether or not
+    # the number at that index is prime.
+    bitarray = [0] * (n + 1)
+    # Follow the algorithm for the Sieve of Eratosthenes (starting with the
+    # first prime, 2)...
+    prime = 2
+    while prime < n:
+        # 1. Take the next available unmarked number in your list (it is prime)
+        if bitarray[prime] == 0:
+            i = 2
+            # 2. Mark all the multiples of that number (they are not prime)
+            while prime * i <= n:
+                bitarray[prime * i] = 1
+                i += 1
+        prime += 1
+    # Reduce the results (ignoring 0 and 1). Return the index of all entries
+    # that have not yet been touched.
+    return [idx for idx, p in enumerate(bitarray) if idx > 1 and p == 0]
