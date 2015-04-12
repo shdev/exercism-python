db4988ab6d13054ef29f9e999e6e580da6d70d9e
Sebastian Holtz <mail@sh-dev.de>
2015-04-12 08:48:24 +0200

    etl goes oneliner

--- etl/etl.py
+++ etl/etl.py
@@ -1,7 +1,4 @@
 def transform(old_style):
-    return_value = {}
-    for key, value_list in old_style.items():
-        for value in value_list:
-            return_value[value.lower()] = key
-
-    return return_value
+    return {value.lower(): key
+            for key, value_list in old_style.items()
+            for value in value_list}
